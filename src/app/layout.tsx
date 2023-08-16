9086e69467d8f35866f67640bb88aefd961df58b
Ryan Skolnick <rhskolnick@gmail.com>
2023-08-16 10:21:39 -0400

    Fixed css that was broken on shadcn/ui dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,36 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Outfit } from 'next/font/google';
-import { cn } from '@/lib/utils';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-import Navbar from '@/components/Navbar';
-import { Toaster } from '@/components/ui/toaster';
-import Providers from '@/components/Providers';
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
 
-const outfit = Outfit({ subsets: ['latin'] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'Casa Bella Quotes',
-    description: 'Quote generator for Casa Bella Outdoor',
+	title: "Casa Bella Quotes",
+	description: "Quote generator for Casa Bella Outdoor",
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <html
-            lang="en"
-            className={cn(
-                outfit.className,
-                'bg-white text-gray-900 subpixel-antialiased'
-            )}
-        >
-            <body className="min-h-screen bg-gradient-to-tr from-black via-indigo-900 to-black subpixel-antialiased text-slate-100">
-                <Providers>
-                    <Navbar />
-                    {children}
-                    <Toaster />
-                </Providers>
-            </body>
-        </html>
-    );
+	return (
+		<html
+			lang='en'
+			className={cn(outfit.className, "subpixel-antialiased")}
+		>
+			<body className='min-h-screen bg-gradient-to-tr from-black via-indigo-900 to-black subpixel-antialiased text-slate-100'>
+				<Providers>
+					<Navbar />
+					{children}
+					<Toaster />
+				</Providers>
+			</body>
+		</html>
+	);
 }
