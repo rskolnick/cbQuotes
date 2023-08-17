bb34eaf070bc3fab81a3c67b9fee8aeb1470b366
Ryan Skolnick <rhskolnick@gmail.com>
2023-08-17 10:07:20 -0400

    Working on updated productsonquotes

--- src/app/(add)/quotes/[id]/components/data-table.tsx
+++ src/app/(add)/quotes/[id]/components/data-table.tsx
@@ -1,149 +1,176 @@
-"use client";
+'use client';
 
 import {
-	ColumnDef,
-	ColumnFiltersState,
-	flexRender,
-	getCoreRowModel,
-	getFilteredRowModel,
-	getPaginationRowModel,
-	useReactTable,
-} from "@tanstack/react-table";
+    ColumnDef,
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
 
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
 interface DataTableProps<TData, TValue> {
-	columns: ColumnDef<TData, TValue>[];
-	data: TData[];
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
 }
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
 export function DataTable<TData, TValue>({
-	columns,
-	data,
+    columns,
+    data,
 }: DataTableProps<TData, TValue>) {
-	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-	const [rowSelection, setRowSelection] = useState({});
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+    const [rowSelection, setRowSelection] = useState({});
 
-	const table = useReactTable({
-		data,
-		columns,
-		getCoreRowModel: getCoreRowModel(),
-		getPaginationRowModel: getPaginationRowModel(),
-		onColumnFiltersChange: setColumnFilters,
-		getFilteredRowModel: getFilteredRowModel(),
-		onRowSelectionChange: setRowSelection,
-		state: {
-			columnFilters,
-			rowSelection,
-		},
-	});
+    const {} = useMutation({
+        mutationFn: async () => {
+            const payload: any = [];
+            table.getRowModel().rows.map((row) => {
+                if (row.getIsSelected()) {
+                    payload.push(row.original);
+                }
+            });
+        },
+    });
 
-	return (
-		<div>
-			<div className='flex items-center py-4 text-slate-600'>
-				<Input
-					placeholder='Filter products...'
-					value={
-						(table
-							.getColumn("productName")
-							?.getFilterValue() as string) ?? ""
-					}
-					onChange={(event) =>
-						table
-							.getColumn("productName")
-							?.setFilterValue(event.target.value)
-					}
-					className='max-w-sm'
-				/>
-			</div>
-			<div className='rounded-md border bg-slate-600'>
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => {
-									return (
-										<TableHead
-											key={header.id}
-											className='text-slate-200'
-										>
-											{header.isPlaceholder
-												? null
-												: flexRender(
-														header.column.columnDef
-															.header,
-														header.getContext()
-												  )}
-										</TableHead>
-									);
-								})}
-							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody className='bg-slate-500'>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow
-									key={row.id}
-									data-state={
-										row.getIsSelected() && "selected"
-									}
-								>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell
-											key={cell.id}
-											className='bg-slate-400'
-										>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext()
-											)}
-										</TableCell>
-									))}
-								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell
-									colSpan={columns.length}
-									className='h-24 text-center'
-								>
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
-			<div className='flex items-center justify-end space-x-2 py-4'>
-				<Button
-					variant='outline'
-					size='sm'
-					onClick={() => table.previousPage()}
-					disabled={!table.getCanPreviousPage()}
-				>
-					Previous
-				</Button>
-				<Button
-					variant='outline'
-					size='sm'
-					onClick={() => table.nextPage()}
-					disabled={!table.getCanNextPage()}
-				>
-					Next
-				</Button>
-			</div>
-		</div>
-	);
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        onRowSelectionChange: setRowSelection,
+        state: {
+            columnFilters,
+            rowSelection,
+        },
+    });
+
+    return (
+        <div>
+            <div className="flex items-center py-4 text-slate-600">
+                <Input
+                    placeholder="Filter products..."
+                    value={
+                        (table
+                            .getColumn('productName')
+                            ?.getFilterValue() as string) ?? ''
+                    }
+                    onChange={(event) =>
+                        table
+                            .getColumn('productName')
+                            ?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
+            <div className="rounded-md border bg-slate-600">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead
+                                            key={header.id}
+                                            className="text-slate-200"
+                                        >
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                      header.column.columnDef
+                                                          .header,
+                                                      header.getContext()
+                                                  )}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody className="bg-slate-500">
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={
+                                        row.getIsSelected() && 'selected'
+                                    }
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell
+                                            key={cell.id}
+                                            className="bg-slate-400"
+                                        >
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
+            <Button
+                className="w-full"
+                onClick={() => {
+                    // const payload: any = [];
+                    // table.getRowModel().rows.map((row) => {
+                    //     if (row.getIsSelected()) {
+                    //         console.log(row.original);
+                    //         payload.push(row.original);
+                    //     }
+                    // });
+                    // console.log(payload);
+                }}
+            >
+                Add Products
+            </Button>
+        </div>
+    );
 }
--- src/app/(add)/quotes/[id]/page.tsx
+++ src/app/(add)/quotes/[id]/page.tsx
@@ -1,96 +1,96 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from '@/components/ui/button';
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog";
-import { Separator } from "@/components/ui/separator";
-import { db } from "@/lib/db";
-import Link from "next/link";
-import { DataTable } from "./components/data-table";
-import { columns } from "./components/columns";
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import { Separator } from '@/components/ui/separator';
+import { db } from '@/lib/db';
+import Link from 'next/link';
+import { DataTable } from './components/data-table';
+import { columns } from './components/columns';
 
 type Props = {
-	params: {
-		id: string;
-	};
+    params: {
+        id: string;
+    };
 };
 export default async function page({ params }: Props) {
-	const { id } = params;
-	const quote = await db.quote.findFirst({
-		where: {
-			id,
-		},
-		include: {
-			dealer: true,
-			products: {
-				select: {
-					productId: true,
-					quantity: true,
-					product: true,
-				},
-			},
-		},
-	});
+    const { id } = params;
+    const quote = await db.quote.findFirst({
+        where: {
+            id,
+        },
+        include: {
+            dealer: true,
+            products: {
+                select: {
+                    productId: true,
+                    quantity: true,
+                    product: true,
+                },
+            },
+        },
+    });
 
-	if (!quote) {
-		return (
-			<div>
-				<h1 className='text-2xl'>No Quote {id}</h1>
-			</div>
-		);
-	}
+    if (!quote) {
+        return (
+            <div>
+                <h1 className="text-2xl">No Quote {id}</h1>
+            </div>
+        );
+    }
 
-	const products = await db.product.findMany();
+    const products = await db.product.findMany();
 
-	return (
-		<div className='container'>
-			<div className='flex items-center justify-between flex-wrap'>
-				<h1 className='text-2xl'>{quote.referenceNum}</h1>
-				<section className='py-4'>
-					<p>{quote.dealer.storeName}</p>
-					<p>{quote.dealer.address}</p>
-					<p>{quote.dealer.email}</p>
-				</section>
-				<Separator />
-				<section>
-					<div className=''>
-						{quote.products.map((productOnQuote) => (
-							<div
-								key={productOnQuote.productId}
-								className='flex items-center justify-normal gap-6 flex-wrap text-lg'
-							>
-								<p className='font-semibold'>
-									{productOnQuote.product.productName}
-								</p>
-								<div className='flex items-center justify-center gap-6 text-slate-400'>
-									<p>${productOnQuote.product.msrp}</p>
-									<p>x {productOnQuote.quantity}</p>
-								</div>
-							</div>
-						))}
-					</div>
-				</section>
-				<Dialog>
-					<DialogTrigger asChild>
-						<Button>Add Products</Button>
-					</DialogTrigger>
-					<DialogContent>
-						<DialogHeader>
-							<DialogTitle>Add Products to Quote</DialogTitle>
-							<DialogDescription>
-								Add products to the quote.
-							</DialogDescription>
-						</DialogHeader>
-						<div className='grid gap-4 py-4'>
-							<DataTable columns={columns} data={products} />
-						</div>
-					</DialogContent>
-				</Dialog>
-			</div>
-		</div>
-	);
+    return (
+        <div className="container">
+            <div className="flex items-center justify-between flex-wrap">
+                <h1 className="text-2xl">{quote.referenceNum}</h1>
+                <section className="py-4">
+                    <p>{quote.dealer.storeName}</p>
+                    <p>{quote.dealer.address}</p>
+                    <p>{quote.dealer.email}</p>
+                </section>
+                <Separator />
+                <section>
+                    <div className="">
+                        {quote.products.map((productOnQuote) => (
+                            <div
+                                key={productOnQuote.productId}
+                                className="flex items-center justify-normal gap-6 flex-wrap text-lg"
+                            >
+                                <p className="font-semibold">
+                                    {productOnQuote.product.productName}
+                                </p>
+                                <div className="flex items-center justify-center gap-6 text-slate-400">
+                                    <p>${productOnQuote.product.msrp}</p>
+                                    <p>x {productOnQuote.quantity}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="mt-6">Add Products</Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Add Products to Quote</DialogTitle>
+                        <DialogDescription>
+                            Add products to the quote.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <DataTable columns={columns} data={products} />
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 }
