5d4a6b45525b5683d881caa6f55c2c98c7c1f371
Ryan Skolnick <rhskolnick@gmail.com>
2023-08-21 11:45:11 -0400

    worked on quote detail page

--- src/app/(add)/quotes/[id]/page.tsx
+++ src/app/(add)/quotes/[id]/page.tsx
@@ -48,6 +48,43 @@ export default async function page({ params }: Props) {
 
     const products = await db.product.findMany();
 
+    if (quote.products.length <= 0) {
+        return (
+            <div className="container">
+                <div className="flex items-center justify-between flex-wrap">
+                    <h1 className="text-2xl">{quote.referenceNum}</h1>
+                    <section className="py-4">
+                        <p>{quote.dealer.storeName}</p>
+                        <p>{quote.dealer.address}</p>
+                        <p>{quote.dealer.email}</p>
+                    </section>
+                    <Separator />
+                    <section>
+                        <div className="flex items-center justify-normal gap-6 flex-wrap text-lg">
+                            <h3 className="text-xl">No Products</h3>
+                        </div>
+                    </section>
+                </div>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className="mt-6">Add Products</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Add Products to Quote</DialogTitle>
+                            <DialogDescription>
+                                Add products to the quote.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <DataTable columns={columns} data={products} />
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            </div>
+        );
+    }
+
     return (
         <div className="container">
             <div className="flex items-center justify-between flex-wrap">
@@ -59,7 +96,7 @@ export default async function page({ params }: Props) {
                 </section>
                 <Separator />
                 <section>
-                    <div className="">
+                    <div>
                         {quote.products.map((productOnQuote) => (
                             <div
                                 key={productOnQuote.productId}
@@ -71,7 +108,7 @@ export default async function page({ params }: Props) {
                                 <div className="flex items-center justify-center gap-6 text-slate-400">
                                     <p>${productOnQuote.product.msrp}</p>
                                     <p>x {productOnQuote.quantity}</p>
-                                    <p>
+                                    <p className="text-white">
                                         $
                                         {productOnQuote.product.msrp *
                                             productOnQuote.quantity}
